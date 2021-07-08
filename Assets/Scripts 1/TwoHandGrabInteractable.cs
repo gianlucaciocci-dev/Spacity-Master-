ca6a3dd2caa6928fd46c6e15cbac93b08b81dea6
Gianlucadev <cioccigianluca95@gmail.com>
2021-07-08 23:34:40 +0200

    Spawn Weapon Incomplete

--- Assets/Scripts 1/TwoHandGrabInteractable.cs
+++ Assets/Scripts 1/TwoHandGrabInteractable.cs
@@ -6,100 +6,100 @@ using UnityEngine.XR.Interaction.Toolkit;
 [RequireComponent(typeof(Rigidbody))]
 public class TwoHandGrabInteractable : XRGrabInteractable
 {
-    public enum TwoHandRotationType
-    {
-        None,
-        First,
-        Second
-    }
+    //public enum TwoHandRotationType
+    //{
+    //    None,
+    //    First,
+    //    Second
+    //}
 
-    public List<XRSimpleInteractable> SecondHandGrabPoint = new List<XRSimpleInteractable>();
-    private XRBaseInteractor secondinteractor;
-    private Quaternion attachinitialrotation;
-    public TwoHandRotationType twoHandRotationType;
-    public bool SnapToSecondHand = true;
-    private Quaternion InitialRotationOffset;
+    //public List<XRSimpleInteractable> SecondHandGrabPoint = new List<XRSimpleInteractable>();
+    //private XRBaseInteractor secondinteractor;
+    //private Quaternion attachinitialrotation;
+    //public TwoHandRotationType twoHandRotationType;
+    //public bool SnapToSecondHand = true;
+    //private Quaternion InitialRotationOffset;
 
-    void Start()
-    {
-        foreach (var item in SecondHandGrabPoint)
-        {
-            item.onSelectEntered.AddListener(OnSecondHandGrab);
-            item.onSelectExited.AddListener(OnSecondHandRelease);
+    //void Start()
+    //{
+    //    foreach (var item in SecondHandGrabPoint)
+    //    {
+    //        item.onSelectEntered.AddListener(OnSecondHandGrab);
+    //        item.onSelectExited.AddListener(OnSecondHandRelease);
 
-        }
-    }
+    //    }
+    //}
+
+    ////protected override void OnSelectEntered(XRBaseInteractor interactor)
+    ////{
+    ////    base.OnSelectEntered(interactor);
+    ////}
+
+    ////protected override void OnSelectExited(XRBaseInteractor interactor)
+    ////{
+    ////    base.OnSelectExited(interactor);
+    ////}
+
+    //void Update()
+    //{
+
+    //}
+
+    //public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
+    //{
+    //    if (secondinteractor && selectingInteractor)
+    //    {
+    //        if (SnapToSecondHand)
+    //            selectingInteractor.attachTransform.rotation = GetTwoHandRotation();
+    //        else
+    //            selectingInteractor.attachTransform.rotation = GetTwoHandRotation() *InitialRotationOffset;
+           
+    //    }
+    //    base.ProcessInteractable(updatePhase);
+    //}
+
+    //private Quaternion GetTwoHandRotation()
+    //{
+    //    Quaternion targetrotation;
+
+    //    switch (twoHandRotationType)
+    //    {
+    //        case TwoHandRotationType.None:
+    //            targetrotation = Quaternion.LookRotation(secondinteractor.attachTransform.position - selectingInteractor.attachTransform.position); return targetrotation;
+    //        case TwoHandRotationType.First:
+    //            targetrotation = Quaternion.LookRotation(secondinteractor.attachTransform.position - selectingInteractor.attachTransform.position, selectingInteractor.attachTransform.up);return targetrotation;
+    //        case TwoHandRotationType.Second:
+    //            targetrotation = Quaternion.LookRotation(secondinteractor.attachTransform.position - selectingInteractor.attachTransform.position, secondinteractor.attachTransform.up);return targetrotation;
+    //        default:
+    //            return new Quaternion();
+    //    }
+    //}
+
+    //public void OnSecondHandGrab(XRBaseInteractor interactor)
+    //{
+    //    secondinteractor = interactor;
+    //    InitialRotationOffset = Quaternion.Inverse(GetTwoHandRotation()) * selectingInteractor.attachTransform.rotation;
+    //}
+
+    //public void OnSecondHandRelease(XRBaseInteractor interactor)
+    //{
+    //    secondinteractor = null;
+    //}
 
     //protected override void OnSelectEntered(XRBaseInteractor interactor)
     //{
-    //    base.OnSelectEntered(interactor);
+    //    base.OnSelectEntering(interactor);
+    //    attachinitialrotation = interactor.attachTransform.localRotation;
     //}
-
     //protected override void OnSelectExited(XRBaseInteractor interactor)
     //{
-    //    base.OnSelectExited(interactor);
+    //    base.OnSelectExiting(interactor);
+    //    secondinteractor = null;
+    //    interactor.attachTransform.localRotation = attachinitialrotation;
     //}
-
-    void Update()
-    {
-
-    }
-
-    public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
-    {
-        if (secondinteractor && selectingInteractor)
-        {
-            if (SnapToSecondHand)
-                selectingInteractor.attachTransform.rotation = GetTwoHandRotation();
-            else
-                selectingInteractor.attachTransform.rotation = GetTwoHandRotation() *InitialRotationOffset;
-           
-        }
-        base.ProcessInteractable(updatePhase);
-    }
-
-    private Quaternion GetTwoHandRotation()
-    {
-        Quaternion targetrotation;
-
-        switch (twoHandRotationType)
-        {
-            case TwoHandRotationType.None:
-                targetrotation = Quaternion.LookRotation(secondinteractor.attachTransform.position - selectingInteractor.attachTransform.position); return targetrotation;
-            case TwoHandRotationType.First:
-                targetrotation = Quaternion.LookRotation(secondinteractor.attachTransform.position - selectingInteractor.attachTransform.position, selectingInteractor.attachTransform.up);return targetrotation;
-            case TwoHandRotationType.Second:
-                targetrotation = Quaternion.LookRotation(secondinteractor.attachTransform.position - selectingInteractor.attachTransform.position, secondinteractor.attachTransform.up);return targetrotation;
-            default:
-                return new Quaternion();
-        }
-    }
-
-    public void OnSecondHandGrab(XRBaseInteractor interactor)
-    {
-        secondinteractor = interactor;
-        InitialRotationOffset = Quaternion.Inverse(GetTwoHandRotation()) * selectingInteractor.attachTransform.rotation;
-    }
-
-    public void OnSecondHandRelease(XRBaseInteractor interactor)
-    {
-        secondinteractor = null;
-    }
-
-    protected override void OnSelectEntered(XRBaseInteractor interactor)
-    {
-        base.OnSelectEntering(interactor);
-        attachinitialrotation = interactor.attachTransform.localRotation;
-    }
-    protected override void OnSelectExited(XRBaseInteractor interactor)
-    {
-        base.OnSelectExiting(interactor);
-        secondinteractor = null;
-        interactor.attachTransform.localRotation = attachinitialrotation;
-    }
-    public override bool IsSelectableBy(XRBaseInteractor interactor)
-    {
-        bool isalredygrabble = selectingInteractor && !interactor.Equals(selectingInteractor);
-        return base.IsSelectableBy(interactor);
-    }
+    //public override bool IsSelectableBy(XRBaseInteractor interactor)
+    //{
+    //    bool isalredygrabble = selectingInteractor && !interactor.Equals(selectingInteractor);
+    //    return base.IsSelectableBy(interactor);
+    //}
 }
---
+++ Assets/SpawnWeapon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnWeapon : MonoBehaviour
+{
+    [SerializeField]
+    GameObject weapon;
+    [SerializeField]
+    Transform barrel;
+
+    private void Start()
+    {
+        GameObject spawnedweapon = Instantiate(weapon, barrel.position, barrel.rotation);
+        spawnedweapon.GetComponent<Rigidbody>().isKinematic = true;
+        spawnedweapon.GetComponent<Rigidbody>().useGravity = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //if(other.tag =="Hand")
+        //{
+        //    GameObject spawnedweapon = Instantiate(weapon, barrel.position, barrel.rotation);
+        //    spawnedweapon.GetComponent<Rigidbody>().isKinematic = true;
+        //    spawnedweapon.GetComponent<Rigidbody>().useGravity = false;
+        //}
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Weapon")
+        {
+            GameObject spawnedweapon = Instantiate(weapon, barrel.position, barrel.rotation);
+            spawnedweapon.GetComponent<Rigidbody>().isKinematic = true;
+            spawnedweapon.GetComponent<Rigidbody>().useGravity = false;
+        }
+    }
+}
